b1d6d8212733cff7410ae86282cca4be94c4695a
Geoff Yuen <g@GeoffBokPro2011.hitronhub.home>
2020-01-22 21:50:59 -0600

    svg sprites

---
+++ src/js/ajaxsprites.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+
+	function svg_debug(thenode){
+		console.log('symbols:');
+			$(thenode).find("symbol").each(function(){
+                if (this.viewBox.baseVal){
+                    console.log("<svg class='' width='" + this.viewBox.baseVal.width + "' height='" + this.viewBox.baseVal.height + "'><use xlink:href='#" + this.id + "'/></svg>");
+                    $("#sprites").append('<svg class="icon"><use xlink:href="#' + this.id + '" /></svg>');    
+                }
+			});
+	}
+
+	$.get( (typeof apppath === 'undefined' ? "." : apppath) + "/img/sprites.svg", function(data) {
+		$("body").prepend("<div id='svg-inject' style='display: none'>" + new XMLSerializer().serializeToString(data.documentElement) + "</div>")
+	})
+		.done(function(){ svg_debug('#svg-inject') })
+	;
+
+});
